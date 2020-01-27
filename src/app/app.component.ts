70eb9b558d9d8997d8547b99df9320dcc42f987c
Dylan Marino <dylmar@umich.edu>
2020-01-27 16:07:09 -0500

    2.2 Changes B

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: '<h1>My App</h1>h1>'
+  template: '<h1>My App</h1>'
 })
 export class AppComponent {}
 
